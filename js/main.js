3b810e24b9378c60d4899fac3218aaee84bf2acc
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-08-07 23:35:21 -0400

    Add ability to select next and previous drink info from array of returned drinks

--- js/main.js
+++ js/main.js
@@ -1,27 +1,48 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch);
+
+let drinkChoiceArr = [];
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+choice
+  const choice = document.querySelector('input').value;
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+choice;
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
-        data.drinks.forEach((name, i) => {
-          console.log(name)
-          if (choice === name) {
-            document.querySelector('h2').innerText = data.drinks[i].strDrink
-            document.querySelector('img').src = data.drinks[i].strDrinkThumb
-            document.querySelector('h3').innerText = data.drinks[i].strInstructions
-          }
-        })
-        // document.querySelector('h2').innerText = data.drinks[0].strDrink
-        // document.querySelector('img').src = data.drinks[0].strDrinkThumb
-        // document.querySelector('h3').innerText = data.drinks[0].strInstructions
+        drinkChoiceArr = data.drinks;
+
+        document.querySelector('h2').innerText = drinkChoiceArr[0].strDrink;
+        document.querySelector('img').src = drinkChoiceArr[0].strDrinkThumb;
+        document.querySelector('h3').innerText = drinkChoiceArr[0].strInstructions;
       })
       .catch(err => {
           console.log(`error ${err}`)
-      });
+      })
+};
+
+document.getElementById('previous-button').addEventListener('click', displayPreviousDrink);
+document.getElementById('pause-button').addEventListener('click', pauseDrinkDisplay);
+document.getElementById('next-button').addEventListener('click', displayNextDrink);
+
+let currentDrink = 0;
+
+function displayPreviousDrink() {
+
+  currentDrink--;
+
+  document.querySelector('h2').innerText = drinkChoiceArr[currentDrink].strDrink;
+  document.querySelector('img').src = drinkChoiceArr[currentDrink].strDrinkThumb;
+  document.querySelector('h3').innerText = drinkChoiceArr[currentDrink].strInstructions;
+}
+
+function pauseDrinkDisplay() {
+
+}
+
+function displayNextDrink() {
+  currentDrink++;
+
+  document.querySelector('h2').innerText = drinkChoiceArr[currentDrink].strDrink;
+  document.querySelector('img').src = drinkChoiceArr[currentDrink].strDrinkThumb;
+  document.querySelector('h3').innerText = drinkChoiceArr[currentDrink].strInstructions;
 }
